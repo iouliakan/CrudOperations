06a56e120b56c1f1d7fd3b31cba9eede642685a0
iouliakan <iouliakanellopoulou02@gmail.com>
2024-07-10 17:18:10 +0300

    Frontend

---
+++ frontent/src/pages/Home.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import { Link} from 'react-router-dom';
+
+
+export default function Home() {
+
+
+    const [users, setUsers] = useState([]); 
+
+    // const {id} = useParams();
+
+    useEffect(()=>{
+        loadUsers(); 
+    },[]); 
+
+
+    const loadUsers=async()=>{
+        const result =await axios.get("http://localhost:8082/users")
+        setUsers(result.data); 
+    }
+
+    const deleteUser = async (id)=>{
+      await axios.delete(`http://localhost:8082/user/${id}`)
+      loadUsers()
+    }
+
+
+
+  return (
+    <div className='container'> 
+    <div className='py-4'>
+    <table className="table border shadow">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">lastName</th>
+      <th scope="col">Email</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+
+   {
+    users.map((user,index)=>(
+
+      <tr key={user.id}>
+      <th scope="row">{index+1}</th>
+      <td>{user.name}</td>
+      <td>{user.username}</td>
+      <td>{user.email}</td>
+      <td>
+        <Link className='btn btn-primary mx-2' to={`/ViewUser/${user.id}`}>View</Link>
+
+        <Link className='btn btn-dark mx-2' to={`/editUser/${user.id}`}
+        >
+        Edit
+        </Link>
+        <button className='btn btn-danger mx-2'
+        onClick={()=> deleteUser(user.id)}
+
+        >Delete</button>
+      </td>
+    </tr>
+    ))
+   }
+
+
+    
+   
+  </tbody>
+</table>
+    </div>
+      
+    </div>
+  )
+}
